e3ae2351fad4297b3722239a139071368b9c44b7
darknorth <ramandy007@gmail.com>
2019-05-01 12:49:15 +0530

    with navbar

---
+++ src/components/form.jsx
@@ -0,0 +1,13 @@
+import React ,{Component}from 'react';
+import Navbar from 'react-bootstrap';
+
+
+class PageForm extends Component{
+    render(){
+        return(
+            <div>hello world</div>
+        );
+    }
+}
+
+export default PageForm;
